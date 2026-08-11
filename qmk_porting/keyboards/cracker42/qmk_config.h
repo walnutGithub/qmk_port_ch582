bd6c8c063995e9f559789e5926e0826b92357e6e
walnutGithub <65359752+walnutGithub@users.noreply.github.com>
2026-08-12 01:12:38 +0800

    Add files via upload

--- qmk_porting/keyboards/cracker42/qmk_config.h
+++ qmk_porting/keyboards/cracker42/qmk_config.h
@@ -19,8 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0001
+#define VENDOR_ID    0xCB10
+#define PRODUCT_ID   0x144C
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Walnut
 #define PRODUCT      cracker42
